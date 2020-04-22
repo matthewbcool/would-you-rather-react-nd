0d6a897d0a45084d7eacc4b0fd3cc2b8790b1c8f
Matt Cool <matthewbcool@gmail.com>
2020-04-22 11:42:34 -0500

    fix: nuke poll mode

--- src/features/home/DisplayAnswer.js
+++ src/features/home/DisplayAnswer.js
@@ -7,7 +7,6 @@ import { useDispatch, useSelector } from 'react-redux';
 const DisplayAnswer = (props) => {
 	let { id } = useParams();
 	let dispatch = useDispatch();
-	const [checkedQuestion, setCheckedQuestion] = useState('');
 	let history = useHistory();
 	let answered = useSelector(answers);
 	let user = useSelector(currentUser);
@@ -16,145 +15,34 @@ const DisplayAnswer = (props) => {
 	if (userObj.profile === '') {
 		history.push('/');
 	}
-	let votes = { one: 0, two: 0 };
-	let userChoice = '';
-	let currentQuestion = {};
-	let choiceOneSelected = true;
-	const submit = (e) => {};
 	console.log(userObj);
-	const findThisAnswer = () => {
-		//loop threw the choices in answered to find the string that matches the id
-		answered.forEach((answer) => {
-			let choiceOne = answer.choiceOne.toLowerCase().split(' ').join('');
-			let choiceTwo = answer.choiceTwo.toLowerCase().split(' ').join('');
-			if (choiceOne.includes(id)) {
-				currentQuestion = answer;
-			}
-			if (choiceTwo.includes(id)) {
-				currentQuestion = answer;
-				choiceOneSelected = false;
-			}
-		});
-	};
-	findThisAnswer();
-	//grab the checked answer and log it
-
-	const setVoteForAnsweredList = () => {
-		let choiceOneVotes = props.questionObject.choiceOneVotes;
-		let choiceTwoVotes = props.questionObject.choiceTwoVotes;
-		//set vote numbers
-		votes.one = choiceOneVotes.length;
-		votes.two = choiceTwoVotes.length;
-		//check to see which choice the user voted for
-		choiceOneVotes.forEach((userName) => {
-			if (userName === user) {
-				userChoice = 'one';
-			}
-		});
-		choiceTwoVotes.forEach((userName) => {
-			if (userName === user) {
-				userChoice = 'two';
-			}
-		});
-	};
-	if (props.questionObject) {
-		//set if we are in the view answer list
-		setVoteForAnsweredList();
-	}
-
-	let answerData = id
-		? {
-				profile: currentQuestion.profile,
-				userName: currentQuestion.id,
-				choiceOne: currentQuestion.choiceOne,
-				choiceTwo: currentQuestion.choiceTwo,
-				choiceOneVotes: 'votes',
-				choiceTwoVotes: 'votes',
-				pollMode: true,
-		  }
-		: {
-				profile: props.profile,
-				userName: props.userName,
-				choiceOne: props.choiceOne,
-				choiceTwo: props.choiceTwo,
-				pollMode: false,
-		  };
-
 	return (
 		<div className="unanswered-wrapper">
 			<div className="profile-asking-wrapper">
-				<h3>{`${answerData.userName} asks:`}</h3>
-				<img className="profile-pic" src={answerData.profile} alt={`${answerData.userName}-asks`} />
+				<h3>{`asks:`}</h3>
+				<img className="profile-pic" src={''} />
 			</div>
 			<h2>Would you rather...</h2>
-			<form name="question" onSubmit={submit}>
+			<form name="question">
 				<div className="input-item">
-					{answerData.pollMode ? (
-						<Fragment>
-							<h4 className={choiceOneSelected ? 'selected' : ''}>{answerData.choiceOne}</h4>
-							{choiceOneSelected ? (
-								<Fragment>
-									<span className="poll-choice-text">{'<--- You voted!'}</span>
-									<span className="votes">Votes: {answerData.choiceOneVotes}</span>
-								</Fragment>
-							) : (
-								<span className="votes">Votes: {answerData.choiceOneVotes}</span>
-							)}
-						</Fragment>
-					) : (
-						<Fragment>
-							<input
-								type="radio"
-								id={answerData.choiceOne}
-								name="question"
-								value={answerData.choiceOne}
-								defaultChecked={userChoice === 'one' ? true : false}
-								disabled={userChoice === 'one' || userChoice === 'two' ? true : false}
-							/>
-							<label htmlFor={answerData.choiceOne}>{answerData.choiceOne}</label>
-						</Fragment>
+					<Fragment>
+						<input type="radio" id="input-one" name="question" value="" />
+						<label htmlFor="">''</label>
+					</Fragment>
 					)}
 				</div>
 				<div className="input-item">
-					{answerData.pollMode ? (
-						<Fragment>
-							<h4 className={choiceOneSelected ? '' : 'selected'}>{answerData.choiceTwo}</h4>{' '}
-							{choiceOneSelected ? (
-								<span className="votes">Votes: {answerData.choiceTwoVotes}</span>
-							) : (
-								<Fragment>
-									<span className="poll-choice-text">{'<--- You voted!'}</span>
-									<span className="votes">Votes: {answerData.choiceTwoVotes}</span>
-								</Fragment>
-							)}
-						</Fragment>
-					) : (
-						<Fragment>
-							<input
-								type="radio"
-								id={answerData.choiceTwo}
-								name="question"
-								value={answerData.choiceTwo}
-								defaultChecked={userChoice === 'two' ? true : false}
-								disabled={userChoice === 'one' || userChoice === 'two' ? true : false}
-							/>
-							<label htmlFor={answerData.choiceTwo}>{answerData.choiceTwo}</label>
-						</Fragment>
+					<Fragment>
+						<input type="radio" id="input-two" name="question" value="" />
+						<label htmlFor=""></label>
+					</Fragment>
 					)}
 				</div>
-				{answerData.pollMode ? (
-					<Link to={`/`}>
-						<button type="submit" className="submit-btn">
-							Back
-						</button>
-					</Link>
-				) : (
-					<Link to={`/questions/${id}`}>
-						<button onClick={submit} type="submit" className="submit-btn">
-							View Poll
-						</button>
-					</Link>
-				)}
+				<Link to={`/`}>
+					<button type="submit" className="submit-btn">
+						Back
+					</button>
+				</Link>
 			</form>
 		</div>
 	);
